0b46ca78e5264ca5cf83fd0a9664dd7b74593aeb
akhtar02 <akhtarhussain0788@gmail.com>
2021-03-30 10:13:48 +0500

    json Server learning

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+const container = document.querySelector ('.blogs')
+
+
+
+const renderPosts = async () => {
+    
+    let uri = ' http://localhost:3000/post';
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+    
+    let templete = '';
+    posts.forEach(post => {
+        templete += `
+        <div class="post">
+        <h2> ${post.title}</h2>
+        <small>${post.likes} Likes</small>
+        <p>${post.body.slice(0, 200)}</p>
+
+        < a href="/details.html"> Read more... </a>
+        </div>
+        `
+    })
+
+
+    container.innerHTML = templete;
+}
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
